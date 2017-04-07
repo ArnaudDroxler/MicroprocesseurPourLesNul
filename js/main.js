e5d33f32bdffa6500eaf1fffdb8d194d0566dab8
arnaud droxler <arnaud.droxler@gmail.com>
2017-04-07 14:52:28 +0200

    split horital and vertical with css

--- js/main.js
+++ js/main.js
@@ -2,15 +2,27 @@ var canvas;
 
 function init(){
     canvas = document.getElementById('canvas');
+
     drawMicroProcesseur();
 }
 
 function  drawMicroProcesseur() {
+
+   //background
     $('canvas').drawRect({
-        fillStyle: '#cacaca',
+        fillStyle: '#bfbfbf',
         x:0, y: 0,
         width: canvas.width,
         height: canvas.height,
+        fromCenter: false
+    });
+
+    //Micropreocesseur
+    $('canvas').drawRect({
+        fillStyle: '#fdff3d',
+        x:0, y: 0,
+        width: 100,
+        height: 100,
         fromCenter: false
     });
 
